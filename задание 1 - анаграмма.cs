6f4de8167e47057cd92ad11e1994229aab32e2f0
Гусев Роман Викторович <91760597+gromvig@users.noreply.github.com>
2022-10-02 21:51:50 +0300

    Add files via upload

---
+++ задание 1 - анаграмма.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите строку 1");
+string? a = Console.ReadLine();
+a = a.ToLower();
+Console.WriteLine("Введите строку 2");
+string? b = Console.ReadLine();
+b = b.ToLower();
+a = String.Concat(a.OrderBy(c => c));
+b = String.Concat(b.OrderBy(c => c));
+if (a == b)
+{
+    Console.WriteLine("Yes");
+}
+else
+{
+    Console.WriteLine("NO");
+}
